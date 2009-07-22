f74f678ba3752fbbddbfc2ef60686a971494f56e
visionmedia <tj@vision-media.ca>
2009-07-22 12:15:16 -0700

    Adding misc params and defaults

--- lib/google-search/search.rb
+++ lib/google-search/search.rb
@@ -3,6 +3,40 @@ module Google
   class Search
     
     ##
+    # Version. Defaults to 1.0
+    
+    attr_accessor :version
+    
+    ##
+    # Offset. Defaults to 0
+    
+    attr_accessor :offset
+    
+    ##
+    # Language. Defaults to :en
+    
+    attr_accessor :lang
+    
+    ##
+    # Query. Defaults to nil
+    
+    attr_accessor :query
+    
+    ##
+    # API Key.
+    
+    attr_accessor :api_key
+    
+    ##
+    # Size. Defaults to :small
+    #
+    #  - :small = 4
+    #  - :large = 8
+    #
+    
+    attr_accessor :size
+    
+    ##
     # Type symbol:
     #
     #   - :local
@@ -17,8 +51,14 @@ module Google
     
     attr_reader :type
     
-    def initialize type
+    def initialize type, options = {}
       @type = type
+      @version = options[:version] || 1.0
+      @offset = options[:offset] || 0
+      @size = options[:size] || :small
+      @lang = options[:lang] || :en
+      @query = options[:query]
+      @api_key = options[:api_key]
     end
   end
 end
