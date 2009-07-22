ab990475ce776a1117c01a8bc13f3848c94d06f0
visionmedia <tj@vision-media.ca>
2009-07-22 14:00:21 -0700

    Misc

---
+++ lib/google-search/item/web.rb
@@ -0,0 +1,48 @@
+
+module Google
+  class Search
+    class Item
+      class Web < Item
+        
+        ##
+        # Unformatted page title.
+        
+        attr_reader :title
+        
+        ##
+        # Absolute uri.
+        
+        attr_reader :uri
+        
+        ##
+        # Cached uri.
+        
+        attr_reader :cached_uri
+        
+        ##
+        # Visible uri.
+        
+        attr_reader :visible_uri
+        
+        ##
+        # Unescaped uri.
+        
+        attr_reader :unescaped_uri
+        
+        ##
+        # Description body.
+        
+        attr_reader :description
+
+        def initialize hash
+          @title = hash[:title]
+          @uri = hash[:url]
+          @cached_uri = hash[:cachedUrl]
+          @visibile_uri = hash[:visibleUrl]
+          @unescaped_uri = hash[:unescapedUrl]
+          @description = hash[:content]
+        end
+      end
+    end
+  end
+end
