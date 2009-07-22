e9317ebc39ee9e2e2011de449c54ffd7a4c17032
visionmedia <tj@vision-media.ca>
2009-07-22 12:02:08 -0700

    Added #type

--- lib/google-search/search.rb
+++ lib/google-search/search.rb
@@ -2,5 +2,23 @@
 module Google
   class Search
     
+    ##
+    # Type symbol:
+    #
+    #   - :local
+    #   - :web
+    #   - :video
+    #   - :blog
+    #   - :news
+    #   - :image
+    #   - :book
+    #   - :patent
+    #
+    
+    attr_reader :type
+    
+    def initialize type
+      @type = type
+    end
   end
 end
--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -2,7 +2,13 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe Google::Search do
-  it "should work" do
-    
+  before :each do
+    @search = Google::Search.new :web
+  end
+  
+  describe "#initialize" do
+    it "should accept the type of search" do
+      @search.type.should == :web
+    end
   end
 end
