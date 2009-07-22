d7f714f085be6d029c9c90177052074868326a40
visionmedia <tj@vision-media.ca>
2009-07-22 16:37:37 -0700

    Fixture helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,11 @@
 $:.unshift File.dirname(__FILE__) + '/../lib'
 require 'rubygems'
 require 'google-search'
+
+def fixture path
+  File.read File.dirname(__FILE__) + "/fixtures/#{path}"
+end
+
+def json_fixture name
+  JSON.parse fixture("#{name}.json")
+end
