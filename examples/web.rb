002e709d4a4b455793efd50f7b89fe0db1856b95
visionmedia <tj@vision-media.ca>
2009-07-23 13:25:05 -0700

    Added 400-response fixture

--- examples/web.rb
+++ examples/web.rb
@@ -5,13 +5,16 @@ require 'google-search'
 require 'rext/all'
 
 
-def page_index_for uri, search
-  search.next.response.items.find do |item|
-    if item.uri == uri
+def page_index_for uri, options = {}
+  @search = Google::Search.new(:web, options) unless options.empty?
+  puts "... Searching page %d for `%s'" % [(@search.offset / 8) + 1, @search.query.url_decode]
+  @search.next.response.items.find do |item|
+    if item.uri =~ uri
       item.index
     end
-  end || page_index_for(uri, search)
+  end || page_index_for(uri)
 end
 
-search = Google::Search.new :web, :size => :large, :query => 'Victoria Web Design'.url_encode
-p page_index_for('http://vision-media.ca/', search)
+p page_index_for(/vision-media/, :size => :large, :query => 'Victoria Web Design'.url_encode)
+p page_index_for(/victoriawebschool/, :size => :large, :query => 'Victoria Web School'.url_encode)
+p page_index_for(/victoriawebschool/, :size => :large, :query => 'Web Design School'.url_encode)
